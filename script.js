e0d2caaaba5da3b4af0c0958aa4ba51fe83b980a
Noobwastaken99 <125329223+Noobwastaken99@users.noreply.github.com>
2023-03-02 20:00:54 -0600

    made active people green

--- script.js
+++ script.js
@@ -5,7 +5,22 @@ window.addEventListener("load", async function fetchData() {
     console.log(data);
 
     for (let i = 0; i < data.length; i++) {
-        container.innerHTML += `
+        if (data[i].active === true) {
+            container.innerHTML += `
+        <div class="astronaut">
+            <div class="bio">
+            <h3>${data[i].id}: ${data[i].firstName} ${data[i].lastName}</h3>
+                <ul>
+                    <li>Hours in Space: ${data[i].hoursInSpace}</li>
+                    <li id="text-color">Active: ${data[i].active}</li>
+                    <li>Skills: ${data[i].skills}</li>
+                </ul>
+            </div>
+            <img class="avatar" src="${data[i].picture}" alt="Astronaut">
+        </div>
+        `;
+        } else {
+            container.innerHTML += `
         <div class="astronaut">
             <div class="bio">
             <h3>${data[i].id}: ${data[i].firstName} ${data[i].lastName}</h3>
@@ -18,5 +33,7 @@ window.addEventListener("load", async function fetchData() {
             <img class="avatar" src="${data[i].picture}" alt="Astronaut">
         </div>
         `;
+        }
+        
     };
 });
