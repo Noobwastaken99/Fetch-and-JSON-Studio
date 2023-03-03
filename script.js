f5c199dc0322200ab1b03680efaf6a94c9a88368
Noobwastaken99 <125329223+Noobwastaken99@users.noreply.github.com>
2023-03-02 18:37:47 -0600

    fetching the data

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 // TODO: add code here
+window.addEventListener("load", async function fetchData() {
+    const response = await fetch('https://handlers.education.launchcode.org/static/astronauts.json');
+    const data = await response.json();
+});
